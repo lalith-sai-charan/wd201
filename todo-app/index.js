e24ecb146c5012c4a3120eb562f4f1f7cf80243c
lalith-sai-charan <lalithsaicharan9@gmail.com>
2024-01-14 08:42:38 +0530

    Your commit message

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
-app.get('/', function (request, response) {
-    response.send('Hello World')
+
+app.get('/todos', function (request, response) {
+    // response.send('Hello World')
+    console.log("Todo List")
   })
+
+  app.post('/todos', function (request, response) {
+    // response.send('Hello World')
+    console.log("Creating a todo ", request.body)
+  })
+
+  app.put('/todos/:id/markAsComplete', function (request, response) {
+    // response.send('Hello World')
+    console.log("We have to update a todo with id", request.params.id)
+  })
+
+  app.delete('/todos/:id', function (request, response) {
+    // response.send('Hello World')
+    console.log("Delete a todo by id", request.params.id)
+  })
+
+
 
 app.listen(3000)
